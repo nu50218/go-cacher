c96a351f4c90522176b97c1ca872af85e9d37125
nu50218 <nu_ll@icloud.com>
2020-03-07 17:54:11 +0900

    fix example

--- _example/main.go
+++ _example/main.go
@@ -1,23 +1,41 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 	"time"
 
 	"github.com/nu50218/go-cacher"
 )
 
-func work(resp *http.Response) {
+func workWithBody(body []byte) error {
 	// do something
+	return nil
+}
+
+func work(c cacher.Cacher) error {
+	val := c.Load()
+	if err, ok := val.(error); ok {
+		return err
+	}
+	body := val.([]byte)
+	return workWithBody(body)
 }
 
 func main() {
 	c := cacher.New(5*time.Minute, func() interface{} {
-		resp, _ := http.Get("hogehoge")
-		return resp
+		resp, err := http.Get("hogehoge")
+		if err != nil {
+			return err
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		return body
 	})
 
 	for i := 0; i < 100; i++ {
-		go work(c.Load().(*http.Response))
+		go work(c)
 	}
 }
